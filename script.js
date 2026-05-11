6bb39df26dd130c6f586fcbedccd3e13d5bd9622
njtsb1 <njtsb1@gmail.com>
2026-05-11 16:03:33 -0300

    adding project - Creating Your First Cryptocurrency on the Ethereum Network

--- script.js
+++ script.js
@@ -320,6 +320,7 @@ const translations = {
     "projects.github.p134": "Creating a Sales and Profit Report with Data Analytics using Power BI",
     "projects.github.p135": "Creating Dynamic Reports using Parameters in Power BI",
     "projects.github.p136": "Creating and Using Your Cryptocurrency Wallet",
+    "projects.github.p137": "Creating Your First Cryptocurrency on the Ethereum Network",
     
     "footer.social": "My Social Networks",
     "footer.powered": "Powered by w3.css"
@@ -621,6 +622,7 @@ const translations = {
     "projects.github.p134": "Criando um Relatório Vendas e Lucros com Data Analytics com Power BI",
     "projects.github.p135": "Criando Relatórios Dinâmicos com o uso de Parâmetros no Power BI",
     "projects.github.p136": "Criando e Utilizando a Sua Carteira de Criptomoedas",
+    "projects.github.p137": "Criando a Sua Primeira Criptomoeda da Rede Ethereum",
 
     "footer.social": "Minhas Redes Sociais",
     "footer.powered": "Desenvolvido com w3.css"
@@ -922,6 +924,7 @@ const translations = {
     "projects.github.p134": "Creación de un informe de ventas y beneficios con análisis de datos mediante Power BI",
     "projects.github.p135": "Creación de informes dinámicos mediante parámetros en Power BI",
     "projects.github.p136": "Creación y uso de su billetera de criptomonedas",
+    "projects.github.p137": "Cómo crear tu primera criptomoneda en la red Ethereum",
     
     "footer.social": "Mis Redes Sociales",
     "footer.powered": "Desarrollado con w3.css"
